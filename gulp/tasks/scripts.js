ee480fb90019c2945327530373d8fd70b8ff0daf
Pierre Burel <pierre.burel@gmail.com>
2017-03-10 16:50:38 +0100

    Added gulp-plumber && gulp-notify

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,4 +1,6 @@
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
 import webpack from 'webpack';
 import webpackStream from 'webpack-stream';
 import named from 'vinyl-named';
@@ -6,7 +8,8 @@ import {scripts as config} from '../config';
 
 gulp.task('scripts', config.deps, (cb) => {
   return gulp.src(config.src)
+    .pipe(plumber())
     .pipe(named())
-    .pipe(webpackStream(config.webpack, webpack))
+    .pipe(webpackStream(config.webpack, webpack).on('error', notify.onError({title: 'Webpack error'})))
     .pipe(gulp.dest(config.dest));
 });
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import notify from 'gulp-notify';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import postcss from 'gulp-postcss';
@@ -8,7 +9,7 @@ import {styles as config} from '../config';
 gulp.task('styles', () => {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
-    .pipe(sass(config.sass).on('error', sass.logError))
+    .pipe(sass(config.sass).on('error', sass.logError).on('error', notify.onError({title: 'Sass error'})))
     .pipe(postcss([
       autoprefixer(config.autoprefixer)
     ]))
