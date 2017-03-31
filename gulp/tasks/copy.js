c58c01b653d3837f0cd68ffef5254e4b0c4b8e49
Pierre Burel <pierre.burel@gmail.com>
2017-03-31 12:38:22 +0200

    Added gulp-changed

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp';
+import changed from 'gulp-changed';
 import {copy as config} from '../config';
 
 gulp.task('copy', config.deps, () => 
   gulp.src(config.src)
+    .pipe(changed(config.dest))
     .pipe(gulp.dest(config.dest))
 );
--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp';
+import changed from 'gulp-changed';
 import {fonts as config} from '../config';
 
 gulp.task('fonts', () => 
   gulp.src(config.src)
+    .pipe(changed(config.dest))
     .pipe(gulp.dest(config.dest))
 );
